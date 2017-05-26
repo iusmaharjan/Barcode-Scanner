8db7b13762f1c00bdd50a5200660189a4ce9fc5c
Ayush Maharjan <ius.maharjan@gmail.com>
2017-05-26 10:44:32 +0545

    Add camera permission check

--- sample/src/main/java/com/lftechnology/barcodescanner/HomeActivity.java
+++ sample/src/main/java/com/lftechnology/barcodescanner/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.lftechnology.barcodescanner;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,6 +19,7 @@ import com.lftechnology.barcodereader.CameraActivity;
 public class HomeActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1000;
+    private static final int RC_HANDLE_CAMERA_PERM = 1001;
 
     private TextView scannedIdTextView;
 
@@ -30,9 +36,47 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivityForResult(new Intent(HomeActivity.this, CameraActivity.class), REQUEST_CODE);
+                int rc = ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.CAMERA);
+                if (rc == PackageManager.PERMISSION_GRANTED) {
+                    startActivityForResult(new Intent(HomeActivity.this, CameraActivity.class), REQUEST_CODE);
+                } else {
+                    requestCameraPermission();
+                }
             }
         });
+    }
+
+    /**
+     * Handles the requesting of the camera permission.  This includes
+     * showing a "Snackbar" message of why the permission is needed then
+     * sending the request.
+     */
+    private void requestCameraPermission() {
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+        builder.setTitle("Need camera Permission");
+        builder.setMessage("This app needs camera permission to detect barcode.");
+        builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.CAMERA}, RC_HANDLE_CAMERA_PERM);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 
     @Override
